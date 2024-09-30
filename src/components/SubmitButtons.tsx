ddc0f2aeec1ae4e972ea62703490ccae56403dac
Sreedharan Namboodiri C <89787284+SridharDigital@users.noreply.github.com>
2024-10-01 02:39:35 +0530

    Refactor settings page component and add user profile functionality

---
+++ src/components/SubmitButtons.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { useFormStatus } from "react-dom";
+
+export const SubmitButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <>
+      {pending ? (
+        <Button disabled className="w-fit">
+          <Loader2 className="mr-2 size-4 animate-spin" /> Please Wait
+        </Button>
+      ) : (
+        <Button className="w-fit" type="submit">
+          Save Now
+        </Button>
+      )}
+    </>
+  );
+};
